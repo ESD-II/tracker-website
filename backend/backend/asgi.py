a7f7c2c2941bba1b594ac2aaf2869b2d73fae413
benf2839 <bdf2768@rit.edu>
2025-05-01 00:13:14 -0400

    fixing backend talking to frontend for replay

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -23,3 +23,17 @@ application = ProtocolTypeRouter({
       )
   ),
 })
+
+
+## backend/asgi.py (TEMPORARY TEST)
+#import os
+#import django
+#from django.core.asgi import get_asgi_application
+
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+#django.setup()
+
+## Only include the basic Django ASGI app for HTTP
+#application = get_asgi_application()
+
+#print("ASGI Application Instantiated (HTTP ONLY TEST) - Daphne is ready.")
